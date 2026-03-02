70dee6fc9ad6ee020d3e74c7387db051dfae7f0e
Pagno-Oria <pagnoinvestimentos@gmail.com>
2026-03-02 20:55:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "Oria está online"}
+
+@app.get("/webhook")
+async def verify(request: Request):
+    params = request.query_params
+    if params.get("hub.mode") == "subscribe":
+        return int(params.get("hub.challenge"))
+    return {"status": "verification failed"}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    print("Mensagem recebida:", data)
+    return {"status": "received"}
